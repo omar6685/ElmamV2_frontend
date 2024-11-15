6cf6c6371f24d47d3a72c0db70ed307c50acf9b6
ZineddineBk09 <eino1209@gmail.com>
2024-11-15 14:51:05 +0100

    nationality report types

---
+++ src/components/dashboard/reports/nationalities/types.d.ts
@@ -0,0 +1,8 @@
+export interface Nationality {
+  name: string;
+  count: number;
+  percentage: number;
+  maxAdditionCount: number;
+  maxAdditionPercentage: number;
+  requiredNumberToAdd: number;
+}
